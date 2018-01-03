a00293bbe799e41655c6541023a89643e4fbe774
yoloasia <34981669+yoloasia@users.noreply.github.com>
2018-01-04 00:48:07 +0100

    comment

--- CoordinatesTransformer.java
+++ CoordinatesTransformer.java
@@ -1,19 +1,25 @@
 package assignment2;
 
-import org.geotools.geometry.jts.JTS;
-import org.geotools.geometry.jts.JTSFactoryFinder;
-import org.geotools.referencing.CRS;
-import org.opengis.referencing.FactoryException;
+import org.geotools.geometry.jts.JTS; //for JTS.transform method
+import org.geotools.geometry.jts.JTSFactoryFinder; //for JTS.FactoryFinder method
+import org.geotools.referencing.CRS; //for CRS methods
+import org.opengis.referencing.crs.CoordinateReferenceSystem; //for defining CRS
+import org.opengis.referencing.operation.MathTransform; //for CRS transform function
+import org.opengis.referencing.FactoryException; 
 import org.opengis.referencing.NoSuchAuthorityCodeException;
-import org.opengis.referencing.crs.CoordinateReferenceSystem;
-import org.opengis.referencing.operation.MathTransform;
 import org.opengis.referencing.operation.TransformException;
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.Coordinate; //for reading coordinate
+import com.vividsolutions.jts.geom.Point; //for creating point
 
 public class CoordinatesTransformer {
 
 	public static void main (String[] args) throws TransformException {
+		
+		/* 
+		 * We used method from CRS class that return a Coordinate Reference System 
+		 * for the specified code to created variable. That way we have defined CRS 
+		 * with name, that we need for transformation
+		*/
 		
 		CoordinateReferenceSystem wgs84 = null;
 		try {
@@ -37,6 +43,10 @@ public class CoordinatesTransformer {
 			e.printStackTrace();
 		}
 		
+		/*
+		 * ready short code that change source CRS into target CRS
+		 */
+		
 		MathTransform wgs84Toutm33n = null;
 		try {
 			wgs84Toutm33n = CRS.findMathTransform(wgs84, utm33);
@@ -47,7 +57,10 @@ public class CoordinatesTransformer {
 		
 		com.vividsolutions.jts.geom.GeometryFactory jtsGf = JTSFactoryFinder.getGeometryFactory();
 		
+		//create point with define variable from assignment
 		Point pointInWgs84 = (Point) jtsGf.createPoint(new Coordinate(13.060209, 47.788901));
+		
+		//transform point from original CRS into new one through created function wgs84Toutm33n
 		Point pointInUtm33n = (Point) JTS.transform(pointInWgs84, wgs84Toutm33n);
 		
 		System.out.println(pointInUtm33n);
